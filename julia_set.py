e68f4bce7cb193ba68c9c37afe3b145260d28458
Danila Fomin <dannyfomin@gmail.com>
2021-04-27 19:33:28 +0300

    commit on 'master'

--- julia_set.py
+++ julia_set.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
 
 def iterations_until_diverge(
         point_of_interest: complex, constant: complex,
@@ -39,3 +39,50 @@ def julia_set(
 
     file_path = "images/" + file_name
     plt.imsave(file_path, matrix.T, cmap=color_map)
+
+
+def julia_set_animation(
+        threshold: int, density: int, constant: complex,
+        color_map="RdPu", file_name="julia_set.gif",
+        frames=120, interval=40):
+    """Julia Set animation.
+    Saves an animation to the animation folder.
+    """
+    real_axis = np.linspace(-2, 2, density)
+    imaginary_axis = np.linspace(-2, 2, density)
+    circle_axis = np.linspace(0, 2*np.pi, frames)
+
+    fig = plt.figure()
+    fig.set_size_inches(10, 10)
+    axes = plt.Axes(fig, [0, 0, 1, 1])
+    axes.set_axis_off()
+    fig.add_axes(axes)
+
+    def animate(i):
+        matrix = np.empty((density, density))
+        current_constant = complex(
+            constant * np.cos(circle_axis[i]),
+            constant * np.sin(circle_axis[i]))
+
+        for row in range(density):
+            for col in range(density):
+                point_of_interest = complex(
+                    real_axis[row],
+                    imaginary_axis[col])
+                matrix[row, col] = iterations_until_diverge(
+                    point_of_interest,
+                    current_constant, threshold)
+
+        interpolation_type = "bicubic"
+        image = axes.imshow(
+            matrix.T, interpolation=interpolation_type,
+            cmap=color_map)
+
+        return [image]
+
+    figure_animation = animation.FuncAnimation(
+        fig, animate, frames=frames,
+        interval=interval, blit=True)
+    file_path = "animations/" + file_name
+    file_writer = "imagemagick"
+    figure_animation.save(file_path, writer=file_writer)
